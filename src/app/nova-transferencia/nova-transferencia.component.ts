9f5de2d376854df4c6d44744c36ccd2cb0440bcf
Ubiratã <100245139+ubirataantunes@users.noreply.github.com>
2022-09-05 12:42:10 -0300

    first commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,28 @@
+import { Component, Output, EventEmitter } from "@angular/core";
+
+@Component({
+    selector: 'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>();
+  @Output() valoresComErro = new EventEmitter<string>();
+
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+    const valorEmitir = {valor: this.valor, destino: this.destino};
+    this.aoTransferir.emit(valorEmitir);
+    this.LimparCampos();
+  }
+
+  LimparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
